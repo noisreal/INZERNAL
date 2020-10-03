d5c3114349bc2222df8850dcca4a265d803f884c
Ama <ama6nen@gmail.com>
2020-10-03 13:26:04 +0300

    Fix typo

--- injector/Properties/AssemblyInfo.cs
+++ injector/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("injector")]
-[assembly: AssemblyDescription("ZINTERNAL injector")]
+[assembly: AssemblyDescription("INZERNAL injector")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("injector")]
-[assembly: AssemblyCopyright("Copyright ama ©  2020")]
+[assembly: AssemblyCopyright("Copyright ama © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
