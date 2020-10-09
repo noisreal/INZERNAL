04aad3652799a386edd0561a319e320f6255327b
Ama <ama6nen@gmail.com>
2020-10-09 11:49:00 +0300

    Add test setting to menu

--- INZERNAL/menu/main_tab.cpp
+++ INZERNAL/menu/main_tab.cpp
@@ -2,6 +2,7 @@
 #include <menu/menu.h>
 
 void menu::main_tab() {
-
-	ImGui::Text("Menu has no features yet.");
+    ImGui::Text("Menu will be updated in later versions.");
+    ImGui::SliderFloat("Punch cooldown", &opt::cheat::punch_cooldown_val, 0.01f, 1.f, "%0.2f");
+	
 }
