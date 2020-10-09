90f4b9f4dbf11f7b103eaf66fb1e09dc85f52726
Ama <ama6nen@gmail.com>
2020-10-09 11:09:13 +0300

    Add imgui

--- INZERNAL/menu/menu.h
+++ INZERNAL/menu/menu.h
@@ -1,7 +1,12 @@
 #pragma once
 #include <core/globals.h>
+#include <menu/imgui/impl/imgui_impl_win32.h>
+#include <menu/imgui/impl/imgui_impl_dx9.h>
 
 //TODO: setup config
+
+//forward declare
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 namespace menu {
 
